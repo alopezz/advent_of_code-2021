348679dcd4e36bdc073c41694fdb784dbce7bbc5
Alex Lopez <alex.lopez-zorzano@betterdoc.de>
2021-12-01 07:24:19 +0100

    Day 1, part 2

--- day1/solver.sql
+++ day1/solver.sql
@@ -16,3 +16,17 @@ WITH consecutive_depths AS (
 )
 SELECT COUNT(*) AS part1_solution FROM consecutive_depths WHERE next > previous;
 
+-- PART 2 --
+
+WITH windowed_depths AS (
+  SELECT id, sum(depth)
+  OVER (ROWS BETWEEN 0 FOLLOWING AND 2 FOLLOWING)
+  FROM aoc_day1_depth
+),
+consecutive_depths AS (
+  SELECT a.sum AS next, b.sum AS previous
+  FROM windowed_depths a
+  JOIN windowed_depths b ON a.id = b.id + 1
+)
+SELECT COUNT(*) AS part2_solution FROM consecutive_depths WHERE next > previous;
+
